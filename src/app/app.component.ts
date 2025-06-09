fdc2282d802ec551d6123536a9472ed3ebffdf7a
TimoRenken <timorenken@outlook.de>
2025-06-09 15:28:38 +0200

    some structure improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderMobileComponent } from './main-content/landing-page/header-mobile/header-mobile.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderMobileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
