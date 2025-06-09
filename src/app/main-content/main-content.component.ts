a30cc043ec4cb78e7ef2a6a9b74364e495b94f7f
TimoRenken <timorenken@outlook.de>
2025-06-09 16:52:54 +0200

    added imprint and footer components

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LandingPageComponent } from './landing-page/landing-page.component'
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [LandingPageComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
